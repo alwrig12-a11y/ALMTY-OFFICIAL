56a8b09c4477eb980c15d27e70fd5016ce74c190
Anthony Wright Jr <alwrig12@gmail.com>
2026-03-24 17:51:25 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.png'; // 1. Import your logo here
 import WaitlistForm from './components/WaitlistForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>ALMTY SOVEREIGN PROTOCOL</h1>
-        <p>INITIALIZING IDENTITY PHASE...</p>
+        {/* 2. Replace the <h1> with your image */}
+        <img src={logo} className="almty-logo-glow" alt="ALMTY Logo" />
+        
+        <p className="protocol-text">INITIALIZING IDENTITY PHASE...</p>
         <WaitlistForm />
       </header>
     </div>
